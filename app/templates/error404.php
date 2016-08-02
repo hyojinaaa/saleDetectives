e20e1341ce6072e97ee426efd8cf05f4dc27151d
Hyojin Jung <hyojinjenny@gmail.com>
2016-08-03 11:29:42 +1200

    Add 404 page

---
+++ app/templates/error404.php
@@ -0,0 +1,18 @@
+<?php 
+
+  $this->layout('master', [
+      'title'=>'Page not found',
+      'desc'=>'page not found'
+    ]);
+
+?>
+
+<!-- Hero section -->
+  <div class="jumbotron">
+    <h2>Sorry, we are lost</h2>
+  </div>
+
+<!-- Main section -->
+<main id="error">
+  <img src="img/lost.jpg">
+</main>
